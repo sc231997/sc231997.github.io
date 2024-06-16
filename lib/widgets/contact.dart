fb84831b953f98d747e4aaf9beb6ded9b473ec80
Chandan Singh <chasing@qti.qualcomm.com>
2024-06-16 18:27:40 +0530

    contact section added

--- lib/widgets/contact.dart
+++ lib/widgets/contact.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class Contact extends StatefulWidget {
   const Contact({Key? key}) : super(key: key);
@@ -10,6 +13,41 @@ class Contact extends StatefulWidget {
 class _ContactState extends State<Contact> {
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Container(
+        margin: EdgeInsets.fromLTRB(0, 100, 0, 250),
+        child: Wrap(alignment: WrapAlignment.spaceEvenly, children: [
+          Container(
+            constraints: BoxConstraints(maxWidth: 600),
+            child: Column(children: [
+              Text(
+                "Get In Touch",
+                style: GoogleFonts.inter(
+                    fontWeight: FontWeight.bold, fontSize: 50),
+              ),
+              Container(height: 20),
+              Text(
+                  "If you are here you must have found something useful. My inbox is always open. Whether you have a question or just want to say hi, I'll try my best to get back to you!",
+                  style: GoogleFonts.inter(fontSize: 17),
+                  textAlign: TextAlign.center),
+              Container(height: 50),
+              OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                    side: BorderSide(
+                        width: 1,
+                        color: Theme.of(context).secondaryHeaderColor)),
+                onPressed: () async {
+                  await launchUrlString("mailto:sc231997@gmail.com");
+                },
+                child: Padding(
+                    padding: EdgeInsets.all(15),
+                    child: Text(
+                      "Say Hi!",
+                      style: GoogleFonts.firaCode(
+                          color: Theme.of(context).secondaryHeaderColor),
+                    )),
+              ),
+            ]),
+          ),
+        ]));
   }
 }
